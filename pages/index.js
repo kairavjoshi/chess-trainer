a0b0ab2b2f228c13216a79421f2133830e2eeea0
kairavjoshi <65003012+kairavjoshi@users.noreply.github.com>
2024-09-19 22:13:42 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,19 @@
 import Head from 'next/head'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 
 const ChessBoard = dynamic(() => import('react-chessboard'), { ssr: false })
 
 export default function Home() {
+  const [boardWidth, setBoardWidth] = useState(400)
+
   useEffect(() => {
     import('../chess-trainer.js')
+    // Adjust board size based on window width
+    const handleResize = () => setBoardWidth(Math.min(400, window.innerWidth - 20))
+    window.addEventListener('resize', handleResize)
+    handleResize()
+    return () => window.removeEventListener('resize', handleResize)
   }, [])
 
   return (
@@ -16,7 +23,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <div id="board" style={{ width: '400px' }}></div>
+        <div id="board" style={{ width: `${boardWidth}px`, margin: '0 auto' }}></div>
         <p id="message"></p>
         <button id="startTraining">Start Training</button>
         <div id="stats">
