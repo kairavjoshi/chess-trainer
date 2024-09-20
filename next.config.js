34258ae1b111c71137e08c813f404dd1a4cbd34e
kairavjoshi <65003012+kairavjoshi@users.noreply.github.com>
2024-09-19 22:24:26 -0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        child_process: false,
+      };
+    }
+    return config;
+  },
+};
