b8aee4008d4d1c1cd259b6c7871bc2d54854d92e
kairavjoshi <65003012+kairavjoshi@users.noreply.github.com>
2024-09-19 22:28:04 -0700

    Update puzzles.js

--- pages/api/puzzles.js
+++ pages/api/puzzles.js
@@ -13,7 +13,7 @@ async function connectToDatabase() {
     useUnifiedTopology: true,
   });
 
-  const db = client.db(process.env.MONGODB_DB);
+  const db = client.db('ChessTrainer');
 
   cachedClient = client;
   cachedDb = db;
@@ -22,13 +22,18 @@ async function connectToDatabase() {
 }
 
 export default async function handler(req, res) {
-  try {
-    const { db } = await connectToDatabase();
-    const collection = db.collection('puzzles');
-    const puzzles = await collection.find({}).toArray();
-    res.status(200).json(puzzles);
-  } catch (error) {
-    console.error('Error fetching puzzles:', error);
-    res.status(500).json({ error: 'Unable to fetch puzzles' });
+  if (req.method === 'GET') {
+    try {
+      const { db } = await connectToDatabase();
+      const collection = db.collection('chess_trainer.puzzles');
+      const puzzles = await collection.find({}).toArray();
+      res.status(200).json(puzzles);
+    } catch (error) {
+      console.error('Error fetching puzzles:', error);
+      res.status(500).json({ error: 'Unable to fetch puzzles' });
+    }
+  } else {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
