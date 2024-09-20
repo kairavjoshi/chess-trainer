fd5bcb6e86a5df15a08649cf12fa610712adacc4
kairavjoshi <65003012+kairavjoshi@users.noreply.github.com>
2024-09-19 22:07:55 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,18 @@
 import Head from 'next/head'
 import { useEffect } from 'react'
+import dynamic from 'next/dynamic'
+
+const ChessBoard = dynamic(() => import('react-chessboard'), { ssr: false })
 
 export default function Home() {
   useEffect(() => {
-    import('/chess-trainer.js')
+    import('../chess-trainer.js')
   }, [])
 
   return (
     <div>
       <Head>
         <title>Chess Trainer</title>
-        <link rel="stylesheet" href="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css" />
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-        <script src="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js"></script>
       </Head>
 
       <main>
