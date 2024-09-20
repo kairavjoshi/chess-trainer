7b5c6376e59a8d94f7df1d5634d85feb2183b731
kairavjoshi <65003012+kairavjoshi@users.noreply.github.com>
2024-09-19 22:17:26 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,35 @@
 import Head from 'next/head'
 import { useEffect, useState } from 'react'
-import dynamic from 'next/dynamic'
-
-const ChessBoard = dynamic(() => import('react-chessboard'), { ssr: false })
 
 export default function Home() {
   const [boardWidth, setBoardWidth] = useState(400)
 
   useEffect(() => {
-    import('../chess-trainer.js')
+    // Load chess-trainer.js
+    const script = document.createElement('script')
+    script.src = '/chess-trainer.js'
+    script.async = true
+    document.body.appendChild(script)
+
     // Adjust board size based on window width
     const handleResize = () => setBoardWidth(Math.min(400, window.innerWidth - 20))
     window.addEventListener('resize', handleResize)
     handleResize()
-    return () => window.removeEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+      document.body.removeChild(script)
+    }
   }, [])
 
   return (
     <div>
       <Head>
         <title>Chess Trainer</title>
+        <link rel="stylesheet" href="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.css" />
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="https://unpkg.com/@chrisoakman/chessboardjs@1.0.0/dist/chessboard-1.0.0.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/chess.js/0.10.3/chess.min.js"></script>
       </Head>
 
       <main>
